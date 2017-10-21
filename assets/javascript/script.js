fba637d5f945469d0d8236ef25f6d4335193a20d
Chase Adams <chaseme37@gmail.com>
2017-10-21 00:51:45 -0600

    finished

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -3,17 +3,95 @@ $(document).ready(function() {
 // GLOBAL VARIABLES
 // ---------------------------------------------------
 var allQuestions = [{
-	question: "Who founded Facebook?",
-	choices: ["Jeff Bezos", "Peter Thiel", "Mark Zuckerberg", "Tom from Myspace"],
-	correctAnswer: 2
+	question: "In the children's book series, where is Paddington Bear originally from?",
+	choices: ["India", "Peru", "Canada", "Iceland"],
+	correctAnswer: 1
   },
 
   {
-  	question: "Which social media platform limits postings to 140 characters?",
-  	choices: ["Twitter", "Facebook", "Instagram", "Snapchat"],
+  	question: "What letter must appear at the beginning of the registration number of all non-military aircraft in the U.S.?",
+  	choices: ["N", "A", "U", "L"],
   	correctAnswer: 0
   
-  }
+  },
+
+  {
+  	question: "Who delivered the less famous two-hour speech that preceded Abraham Lincoln's two-minute Gettysburg Address?",
+  	choices: ["Wendell Phillips", "Daniel Webster", "Robert G. Ingersoll", "Edward Everett"],
+  	correctAnswer: 3
+  },
+
+  {
+  	question: "'Nephelococcygia' is the practice of doing what?",
+  	choices: ["Finding shapes in clouds", "Sleeping with your eyes open", "Breaking glass with your voice", "Swimming in freezing water"],
+  	correctAnswer: 0
+  },
+
+  {
+  	question: "Which insect shorted out an early supercomputer and inspired the term 'computer bug'?",
+  	choices: ["Moth", "Roach", "Fly", "Japanese Beetle"],
+  	correctAnswer: 0
+  },
+
+  {
+  	question: "Which of the following men does not have a chemical element named for him?",
+  	choices: ["Albert Einstein", "Niels Bohr", "Isaac Newton", "Enrico Fermi"],
+  	correctAnswer: 2
+  },
+
+  {
+  	question: "Which of these ships was not one of the three taken over by colonists during the Boston Tea Party?",
+  	choices: ["Eleanor", "Dartmouth", "Beaver", "William"],
+  	correctAnswer: 3
+  },
+
+  {
+  	question: "Now used to refer to a cat, the word 'tabby' is derived from the name of a district of what world capital?",
+  	choices: ["Baghdad", "New Delhi", "Cairo", "Moscow"],
+  	correctAnswer: 0
+  },
+
+  {
+  	question: "Which First Lady was a ninth-generation descendant of Pocahontas?",
+  	choices: ["Helen Taft", "Edith Wilson", "Bess Truman", "Mamie Eisenhower"],
+  	correctAnswer: 1
+  },
+
+  {
+  	question: "For ordering his favorite beverages on demand, LBJ had four buttons installed in the Oval Office labeled 'coffee,' 'tea,' 'Coke,' and what?",
+  	choices: ["Fresca", "V8", "Yoo-hoo", "A&W"],
+  	correctAnswer: 0
+  },
+
+  {
+  	question: "Khrushchev's famous 1960 'shoe-banging' outburst at the U.N. was in response to a delegate from what nation?",
+  	choices: ["Australia", "The Netherlands", "The Philippines", "Turkey"],
+  	correctAnswer: 2
+  },
+
+  {
+  	question: "The most watched TV episode of all time, the final episode of M*A*S*H aired at 8:30 p.m. on February 28, 1983, following what sitcom?",
+  	choices: ["Private Benjamin", "Square Pegs", "Alice", "Newhart"],
+  	correctAnswer: 2
+  },
+
+  {
+  	question: "Which of the following landlocked countries is entirely contained within another country?",
+  	choices: ["Lesotho", "Burkina Faso", "Mongolia", "Luxembourg"],
+  	correctAnswer: 0
+  },
+
+  {
+  	question: "Who did artist Grant Wood use as the model for the farmer in his classic painting 'American Gothic'?",
+  	choices: ["Traveling salesman", "Local sheriff", "His dentist", "His butcher"],
+  	correctAnswer: 2
+  },
+
+  {
+  	question: "Which of these U.S. Presidents appeared on the television series 'Laugh-In'?",
+  	choices: ["Lyndon Johnson", "Richard Nixon", "Jimmy Carter", "Gerald Ford"],
+  	correctAnswer: 1
+  },
 ];
 
 
@@ -24,6 +102,9 @@ var answerOptions = [];
 var questionIndex = 0;
 var countCorrect = 0;
 var countWrong = 0;
+var coolButton = ("<button style='margin-top:30px;' class='set_1_btn Vbtn-1' id='start-button'><svg preserveAspectRatio='xMidYMin'>"
+	+ "<rect x='0' y='0' fill='none' width='100%'' height='100%'></rect></svg>"
+	+ "</button>")
 
 
 // FUNCTIONS
@@ -90,8 +171,8 @@ var gameReset = function() {
 
 var startGame = function() {
 	$("#question").empty();
-	var welcome = $("<h1>").text("Click below to start the game!");
-	var startButton = $("<button class='btn btn-large' id='start-button'>").text("START");
+	var welcome = $("<h1>").text("Can you answer these million dollar questions?");
+	var startButton = $(coolButton).append("START");
 	$("#question").append(welcome, startButton);
 
 	$("#start-button").on("click", function() {
@@ -107,7 +188,7 @@ var renderQuestion = function() {
 		$("#question").append($("<h1>").text("Game Over!"));
 		$("#choices").append($("<h3>").text("Total Correct: " + countCorrect));
 		$("#response").append($("<h3>").text("Total Incorrect: " + countWrong));
-		var playAgain = $("<button class='btn btn-large' id='play-button'>").text("PLAY AGAIN");
+		var playAgain = $("<button style='margin-top:30px;' class='btn btn-warning btn-large' id='play-button'>").text("PLAY AGAIN");
 		$("#response").append(playAgain);
 		myCounter.stop();
 
@@ -118,7 +199,7 @@ var renderQuestion = function() {
 		questionText = $("<h1>").text(currentQuestion.question);
 		$("#question").append(questionText);
 		for (var i = 0; i < currentQuestion.choices.length; i++) {
-			answerOptions = $("<button class='btn btn-large' id='btn-choice'></button><br><br>");
+			answerOptions = $("<button style='margin:15px;' class='btn btn-warning btn-large' id='btn-choice'></button>");
 			answerOptions.addClass("choice");
 			answerOptions.attr("data-index", i);
 			answerOptions.text(currentQuestion.choices[i]);
